2d90d0520f30f51640ec7d88a086c45afd22a924
KamenKoshutanski <kamen2109@gmail.com>
2023-06-17 19:57:07 +0300

    People class basic implementation

--- People/Person.java
+++ People/Person.java
@@ -1,5 +1,41 @@
 package People;
 
+import Vehicles.Base.VehicleBase;
+
 public class Person {
-    
+    private String name;
+    private double budget;
+    private VehicleBase preference;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    public VehicleBase getPreference() {
+        return preference;
+    }
+
+    public void setPreference(VehicleBase preference) {
+        this.preference = preference;
+    }
+
+    public void buy(VehicleBase vb) {
+
+    }
+
+    public void makeOffer(double offerPrice) {
+
+    }
 }
