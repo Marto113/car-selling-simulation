044d2895a4ec6950d5392901a1204d578c3f6546
martin <karabonevmartin@gmail.com>
2023-06-17 19:00:42 +0300

    general car parameters

--- Vehicles/Base/VehicleBase.java
+++ Vehicles/Base/VehicleBase.java
@@ -1,5 +1,19 @@
 package Vehicles.Base;
 
 public class VehicleBase {
-    
+    private String model;
+    private Type type;
+    private Condition condition;
+
+    public String getModel(){
+        return this.model;
+    }
+
+    public String getType(){
+        return type.getType();
+    }
+
+    public String getCondition(){
+        return condition.getCondition();
+    }
 }
