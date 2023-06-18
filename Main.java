7c9e3e935e3d6218f1cb3cc90525f57304dc209b
CatsArePanzers <kristianm2005@gmail.com>
2023-06-18 17:59:17 +0300

    Basic test

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import Vehicles.Base.VehicleBase;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        VehicleBase vb = new VehicleBase();
+
+        System.out.println(vb.getModel());
+        System.out.println(vb.getType());
+        System.out.println(vb.getCondition());
+
     }
 }
