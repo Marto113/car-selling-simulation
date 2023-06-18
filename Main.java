9d9990f57a12f5d8a5f7dbb53ee0d7863f4b1c38
martin <karabonevmartin@gmail.com>
2023-06-19 01:22:52 +0300

    main merge

--- Main.java
+++ Main.java
@@ -1,22 +1,21 @@
+import java.util.HashMap;
+import java.util.Random;
 import java.util.Vector;
 
+import Dealership.DealerShip;
 import Vehicles.*;
 import People.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Vector<VehicleBase> cars = new Vector<VehicleBase>();
+    public static void main(String[] args) throws Exception {  
 
-        for (int i = 0; i < 10; i++) {
-            cars.add(new VehicleBase());
-            cars.add(new ElectricVehicle());
-            cars.add(new HybridVehicle());
+        DealerShip store = new DealerShip();
+
+        while(true) {
+            store.makeVehicle();
+            
+            
         }
 
-        for (VehicleBase i : cars) {
-            System.out.print(i.getModel() + " ");
-            System.out.print(i.getType() + " ");
-            System.out.println(i.getCondition());
-        }
     }
 }
