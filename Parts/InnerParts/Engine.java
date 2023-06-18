9d46e36cc646b8bdfa15e703bd77b658da80b32b
Martin <61360302+Marto113@users.noreply.github.com>
2023-06-18 12:53:17 +0300

    Engine class created

--- Parts/InnerParts/Engine.java
+++ Parts/InnerParts/Engine.java
@@ -1,5 +1,31 @@
 package Parts.InnerParts;
 
 public class Engine {
-    
+    private int numberOfCylinders;
+    private int cubicCapacity;
+    private String fuelType;
+
+    public int getNumberOfCylinders(){
+        return this.numberOfCylinders;
+    }
+
+    public int getCubicCapacity(){
+        return this.cubicCapacity;
+    }
+
+    public String getFuelType(){
+        return this.fuelType;
+    }
+
+    public void setNumberOfCylinders(int c){
+        this.numberOfCylinders = c;
+    }
+
+    public void setCubicCapacity(int cc){
+        this.cubicCapacity = cc;
+    }
+
+    public void setFuelType(String fuel){
+        this.fuelType = fuel;
+    }
 }
--- Vehicles/Base/VehicleBase.java
+++ Vehicles/Base/VehicleBase.java
@@ -1,9 +1,12 @@
 package Vehicles.Base;
 
+import Parts.InnerParts.Engine;
+
 public class VehicleBase {
     private String model;
     private Type type;
     private Condition condition;
+    private Engine engine;
 
     public String getModel(){
         return this.model;
@@ -15,5 +18,26 @@ public class VehicleBase {
 
     public String getCondition(){
         return condition.getCondition();
+    }
+
+    public int getFuelConsumption(){
+        int cylinders = engine.getNumberOfCylinders();
+        int cc = engine.getCubicCapacity();
+        String fuel = engine.getFuelType();
+
+        int consumption = cylinders * cc;
+
+        if (fuel == "Diesel"){
+            consumption /= 2000;
+            return consumption;
+        } else if (fuel == "Petrol"){
+            consumption /= 2200;
+            return consumption;
+        } else if (fuel == "Gas"){
+            consumption /= 3000;
+            return consumption;
+        }
+
+        return consumption / 2000;
     }
 }
